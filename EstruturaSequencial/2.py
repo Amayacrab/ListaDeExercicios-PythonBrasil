511db05bed4de00f0f92909f5e03a69292afc283
Amayacrab <63087780+Amayacrab@users.noreply.github.com>
2022-08-12 00:47:25 -0300

    Add files via upload

---
+++ EstruturaSequencial/2.py
@@ -0,0 +1,3 @@
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+Numero=input('Coloca um numero ae!')
+print('O numero que voce colocou foi ', Numero)
